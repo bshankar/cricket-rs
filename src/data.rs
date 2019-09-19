28092df04f7964717d989b0a3e02669a4e59091b
Bhavani Shankar <ebhavanishankar@gmail.com>
2019-09-19 22:54:49 +0530

    Move player data to data.rs

---
+++ src/data.rs
@@ -0,0 +1,34 @@
+pub struct Player {
+    pub name: &'static str,
+    pub probabilities: Vec<f64>,
+}
+
+impl Player {
+    fn new(name: &'static str, probabilities: Vec<f64>) -> Self {
+        Player {
+            name: name,
+            probabilities: probabilities,
+        }
+    }
+}
+
+pub fn get_player_data() -> Vec<Player> {
+    vec![
+        Player::new(
+            "Kirat Boli ",
+            vec![0.05, 0.3, 0.25, 0.1, 0.15, 0.01, 0.09, 0.05],
+        ),
+        Player::new(
+            "N.S Nodhi ",
+            vec![0.1, 0.4, 0.2, 0.05, 0.1, 0.01, 0.04, 0.1],
+        ),
+        Player::new(
+            "R Rumrah ",
+            vec![0.2, 0.3, 0.15, 0.05, 0.05, 0.01, 0.04, 0.2],
+        ),
+        Player::new(
+            "Shashi Henra ",
+            vec![0.3, 0.25, 0.05, 0.0, 0.05, 0.01, 0.04, 0.3],
+        ),
+    ]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,16 @@
-use std::collections::HashMap;
+mod data;
 mod each_ball;
 mod game_state;
 use game_state::GameState;
 
-fn get_player_data() -> HashMap<&'static str, Vec<f64>> {
-    let mut players = HashMap::new();
-    players.insert(
-        "Kirat Boli",
-        vec![0.05, 0.3, 0.25, 0.1, 0.15, 0.01, 0.09, 0.05],
-    );
-    players.insert("N.S Nodhi", vec![0.1, 0.4, 0.2, 0.05, 0.1, 0.01, 0.04, 0.1]);
-    players.insert(
-        "R Rumrah",
-        vec![0.2, 0.3, 0.15, 0.05, 0.05, 0.01, 0.04, 0.2],
-    );
-    players.insert(
-        "Shashi Henra",
-        vec![0.3, 0.25, 0.05, 0.0, 0.05, 0.01, 0.04, 0.3],
-    );
-    players
-}
-
 fn main() {
     let mut rng = rand::thread_rng();
-    let players = get_player_data();
+    let players = data::get_player_data();
     let game_state = GameState::new();
 
     // Simulate each ball with weighted random number generator
     // Simulate changes after each ball
     // Until game ends
-    println!(
-        "{:?}",
-        each_ball::weighted_pick(players.get("Kirat Boli").unwrap().to_vec(), &mut rng)
-    );
+    let probs = &players[0].probabilities;
+    println!("{:?}", each_ball::weighted_pick(probs, &mut rng));
 }
